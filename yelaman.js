0fe63fb6984fc0e380c467733d18f983425d2f59
Еламан Сураншиев <@itstep.loc>
2022-11-10 21:12:13 +0600

    yelaman

--- yelaman.js
+++ yelaman.js
@@ -1,1 +1,76 @@
-console.log('yelaman');
+let user = [
+    {
+        name: "yelaman",
+        age: 26,
+        mail: "zxcvb@gmail.com",
+        hight: 180,
+        weight: 60
+    },
+    {
+        name: "abylaikhan",
+        age: 30,
+        mail: "asd@gmail.com",
+        hight: 180,
+        weight: 60
+    },
+    {
+        name: "yerdaulet",
+        age: 21,
+        mail: "qwe@gmail.com",
+        hight: 180,
+        weight: 60
+    },
+    {
+        name: "zhangir",
+        age: 24,
+        mail: "asdsd@gmail.com",
+        hight: 180,
+        weight: 60
+    },
+    {
+        name: "aleksandra",
+        age: 15,
+        mail: "zxc@gmail.com",
+        hight: 180,
+        weight: 60
+    },
+    {
+        name: "asel",
+        age: 30,
+        mail: "poi@gmail.com",
+        hight: 180,
+        weight: 60
+    },
+    {
+        name: "fedor",
+        age: 16,
+        mail: "dfg@gmail.com",
+        hight: 180,
+        weight: 60
+    },
+    {
+        name: "aruzhan",
+        age: 23,
+        mail: "qwerty@gmail.com",
+        hight: 180,
+        weight: 60
+    },
+    {
+        name: "symbat",
+        age: 25,
+        mail: "asddsa@gmail.com",
+        hight: 180,
+        weight: 60
+    },
+    {
+        name: "maksim",
+        age: 16,
+        mail: "lkjhg@gmail.com",
+        hight: 180,
+        weight: 60
+    },
+]
+
+localStorage.setItem('user', JSON.stringify(user));
+
+
