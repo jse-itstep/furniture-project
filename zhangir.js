9bfa1c42b54cd2c9b896143c23f5d9ded35cf0bd
Жангир Уакасов <zangiruakasov@gmail.com>
2022-11-10 21:08:15 +0600

    update version

--- zhangir.js
+++ zhangir.js
@@ -1,1 +1,15 @@
-console.log('zhangir');
+
+let music = [
+    { name: "I kissed A girl", genre: "pop", singerName: "Katy Perry", albumName: "first album", year: 2010 },
+    { name: "Bring Me To Life", genre: "rock", singerName: "Evanescence", albumName: "Fallen", year: 2005},
+    { name: "Животные", genre: "rap", singerName: "Скриптонит", albumName: "Уроборос", year: 2016},
+    { name: "Як 2", genre: "rap", singerName: "Скриптонит", albumName: "2004", year: 2018},
+    { name: "Praise The Lord", genre: "niggas rap", singerName: "A$AP Rocky", albumName: "Testing", year: 2016},
+    { name: "I'm Real", genre: "rap", singerName: "Ulukmanapo", albumName: "I'm Real", year: 2020},
+    { name: "Homicide", genre: "rap", singerName: "Eminem, Logic", albumName: "IDK", year: 2022},
+    { name: "В одного", genre: "rap", singerName: "Скриптонит, Truwer", albumName: "Улица 36", year: 2015},
+    { name: "Mama Makusa", genre: "rap", singerName: "Kizaru", albumName: "Bandana", year: 2021},
+]
+
+
+localStorage.setItem(JSON.stringify("mal", animal));
