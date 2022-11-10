89f37c90adeb6ccb3c6cbcfe73fb08a3f31fa500
Аружан <aru@MacBook-Air-Aruzan.local>
2022-11-10 21:14:29 +0600

    aru

--- aru.js
+++ aru.js
@@ -1,1 +1,21 @@
 console.log("aru")
+
+
+let furnitureArray = JSON.parse(localStorage("furniture"))
+let div = document.querySelector(".block")
+
+function drawFurniture(){
+    for(let furniture of furnitureArray){
+        div.innerHTML += `
+            <div class="block">
+                <p>view: ${furniture.view}</p>
+                <p>bolt: ${furniture.bolts}</p>
+                <p>wood: ${furniture.wood}</p>
+                <p>color: ${furniture.color}</p>
+                <p>height: ${furniture.height}</p>
+            </div>
+        `
+    }
+}
+
+drawFurniture()
