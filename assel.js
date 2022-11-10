918be4a48432bb42e3df37e8245c15f399cb5f26
omarovaassel <omarova.assel@gmail.com>
2022-11-10 21:09:51 +0600

    file added

--- assel.js
+++ assel.js
@@ -1,1 +1,19 @@
 console.log('assel');
+
+let carArray = JSON.parse(localStorage.getItem('cars'));
+let div = document.querySelector(".div");
+
+function drawCar(carArray){
+    for(let item of carArray){
+        div.innerHTML += `
+        <p>&#128663;</p>
+        <p>model: ${model}</p>
+        <p>color: ${color}</p>
+        <p>year: ${year}</p>
+        <p>price: ${price}</p>
+        <p>country: ${country}</p>        
+        `
+    }
+};
+
+drawCar(carArray);
