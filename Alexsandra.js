8d2d3adcf8b9da1003f0d538e0ae005ad4ae1b7f
Крюкова Александра Вадимовна <@itstep.loc>
2022-11-10 21:42:59 +0600

    qwe

--- Alexsandra.js
+++ Alexsandra.js
@@ -1,1 +1,109 @@
-console.log('alexsandra')
+// console.log('alexsandra')
+// Диагональ-diagonal
+
+// Тип матрицы-matrixType
+
+
+// Разрешение экрана-screenResolution
+
+// Формат-format
+
+
+// Интерфейс-interface
+
+
+// Яркость матрицы-matrixBrightness
+
+
+
+
+let computer = [
+    {
+        name:"LG 27GN600",
+        diagonal:"27",
+        matrixType:"IPS",
+        screenResolution:"1920x1080 (FHD)",
+        format:"16:9",
+        interface:"1x DisplayPort, 2x HDMI",
+        prise:123.999
+    
+    },
+    
+    {
+        name:"Samsung LF24T350FHIXCI ",
+        diagonal:"24",
+        matrixType:"IPS",
+        screenResolution:"1920x1080 (FHD)",
+        format:"16:9",
+        interface:"1x D-SUB, 1x HDMI",
+        prise:169.990
+    
+    },
+    
+    {
+        name:"LG 27UL500 3840x2160",
+        diagonal:"27",
+        matrixType:"IPS",
+        screenResolution:"3840x2160 (4K Ultra HD)",
+        format:"16:9",
+        interface:"1x DisplayPort, 2x HDMI",
+        prise:199.990
+    
+    },
+    
+    {
+        name:"LG 32UK550",
+        diagonal:"24",
+        matrixType:"VA",
+        screenResolution:"3840x2160",
+        format:"16:9",
+        interface:"1x DisplayPort, 2x HDMI",
+        matrixBrightness:241.990
+    
+    },
+    
+    {
+        name:"Samsung LC27R500FHIXCI",
+        diagonal:"27",
+        matrixType:"IPS",
+        screenResolution:"1920x1080 (FHD)",
+        format:"16:9",
+        interface:"1x DisplayPort, 2x HDMI",
+        matrixBrightness:99.990
+    
+    },
+    {
+        name:"Samsung Smart LS32AM700UIXCI",
+        diagonal:"27",
+        matrixType:"IPS",
+        screenResolution:"1920x1080",
+        format:"16:9",
+        interface:"1x DisplayPort, 2x HDMI",
+        matrixBrightness:165.690
+    
+    },
+    {
+        name:"Samsung LS27AG300NIXCI",
+        diagonal:"27",
+        matrixType:"IPS",
+        screenResolution:"1920x1080",
+        format:"16:9",
+        interface:"1x DisplayPort, 2x HDMI",
+        matrixBrightness:137.990
+    
+    },
+    {
+        name:"LG 27MP400",
+        diagonal:"27",
+        matrixType:"IPS",
+        screenResolution:"1920x1080",
+        format:"16:9",
+        interface:"1x DisplayPort, 2x HDMI",
+        matrixBrightness:82.990
+    
+    }
+    
+    ]
+    
+    localStorage.setItem('computer',JSON.stringify(computer))
+    
