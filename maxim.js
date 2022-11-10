8c252356c53221e4903539c1829891a6112c8f9d
Буянов Максим Геннадьевич <@itstep.loc>
2022-11-10 21:13:04 +0600

    maxim

--- maxim.js
+++ maxim.js
@@ -1,1 +1,19 @@
-console.log("maxim")
+let users = JSON.parse(localStorage.getItem("user")) || []
+let div = document.querySelector("#div")
+
+
+
+function drawUser(){
+    for(let user of users){
+        div.innerHTML += `
+        <br><br><br>
+        <p>Name: ${user.name}</p>
+        <p>Age: ${user.age}</p>
+        <p>Email: ${user.mail}</p>
+        <p>Growth: ${user.hight}</p>
+        <p>Weight: ${user.weight}</p>
+        `
+    }
+}
+
+drawUser()
